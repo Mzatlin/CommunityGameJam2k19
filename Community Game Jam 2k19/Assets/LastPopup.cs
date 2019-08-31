7d8e0cf1e5c1ee4775e2d1421a73ced032b0c3a7
Michael Zatlin <michael.zatlin@yahoo.com>
2019-08-31 09:31:30 -0700

    Ending Scene Finished

---
+++ Community Game Jam 2k19/Assets/LastPopup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastPopup : MonoBehaviour
+{
+    [SerializeField]
+    Canvas canvas;
+    [SerializeField]
+    AudioSource source;
+    [SerializeField]
+    AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.enabled = false;
+        StartCoroutine(Popup());
+        
+    }
+    IEnumerator Popup()
+    {
+        yield return new WaitForSeconds(3);
+        canvas.enabled = true;
+        source.clip = clip;
+        source.Play();
+    }
+
+
+}
